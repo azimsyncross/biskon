a0c9d62b463a146a6e5020b52c7797f0e98209ba
azim128 <azimruet28@gmail.com>
2025-08-11 09:01:07 +0600

    feat(setup): update info

--- src/components/contact/ContactSection.jsx
+++ src/components/contact/ContactSection.jsx
@@ -345,7 +345,7 @@ const ContactPage = () => {
           </p>
           <div className="rounded-xl overflow-hidden shadow-2xl shadow-purple-500/10 border border-gray-700">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3591.377417113341!2d-80.19391772582219!3d25.82410600602593!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9b15ef5d557b7%3A0x4d2a494faa359834!2s5205%20NE%202nd%20Ave%2C%20Miami%2C%20FL%2033137%2C%20USA!5e0!3m2!1sen!2sbd!4v1754844343621!5m2!1sen!2sbd"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3579.5210417839867!2d-80.0948511258069!3d26.212254489843588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d8fd8400cec3f9%3A0xf271c1008e615a74!2s1480%20S%20Ocean%20Blvd%20APT%20109%2C%20Pompano%20Beach%2C%20FL%2033062%2C%20USA!5e0!3m2!1sen!2sbd!4v1754881155307!5m2!1sen!2sbd"
               width="100%"
               height="450"
               style={{ border: 0 }}
